77f80ee43ad5f54f38bf119add7e707a686d749a
alonewalker365 <alonewalker3125@gmail.com>
2026-02-27 15:32:42 +0530

    intial commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import nunjucks from "vite-plugin-nunjucks";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        about: resolve(__dirname, "src/about.html"),
+        contact: resolve(__dirname, "src/contact.html")
+      }
+    }
+  },
+  plugins: [
+    nunjucks({
+      templatesDir: resolve(__dirname, "src/")
+    })
+  ]
+});
